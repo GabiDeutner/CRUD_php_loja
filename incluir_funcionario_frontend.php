937b9db33dcc2d69869b9b68b36936f83b417214
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 20:29:58 -0300

    Create incluir_funcionario_frontend.php

---
+++ incluir_funcionario_frontend.php
@@ -0,0 +1,73 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_funcionario_backend.php"><br>
+<p>
+
+Codigo:
+<input type="text" name="fun_codigo" ><br>
+<br>
+
+Nome:
+<input type="text" name="fun_nome"><br>
+<br>
+
+Endereço:
+<input type="text" name="fun_endereco"><br>
+<br>
+
+Numero:
+<input type="text" name="fun_numero"><br>
+<br>
+
+Complemento:
+<input type="text" name="fun_complemento"><br>
+<br>
+
+Bairro:
+<input type="text" name="fun_bairro"><br>
+<br>
+
+Cidade:
+<input type="text" name="fun_cidade"><br>
+<br>
+
+UF:
+<input type="text" name="fun_uf"><br>
+<br>
+
+CNPJ / CPF:
+<input type="text" name="fun_cnpjcpf"><br>
+<br>
+
+RG:
+<input type="text" name="fun_rgie"><br>
+<br>
+
+Sexo:
+<input type="text" name="fun_sexo"><br>
+<br>
+
+Nascimento:
+<input type="text" name="fun_nascimento"><br>
+<br>
+
+Telefone:
+<input type="text" name="fun_telefone"><br>
+<br>
+
+Celular:
+<input type="text" name="fun_celular"><br>
+<br>
+
+E-mail:
+<input type="text" name="fun_email"><br>
+<br>
+
+Salario:
+<input type="text" name="fun_salario"><br>
+<br>
+
+<input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
