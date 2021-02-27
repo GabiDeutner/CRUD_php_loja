e15c92dc95f0ccb086a601b57f6105050c6e6d4c
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 20:04:17 -0300

    Create incluir_vendas_frontend.php

---
+++ incluir_vendas_frontend.php
@@ -0,0 +1,75 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_vendas_backend.php"><br>
+<p>
+
+
+Codigo:
+<input type="text" name="ven_codigo" ><br>
+<br>
+
+
+
+<br> Codigo funcionário: <select size="1" name="fun_codigo">
+<?php
+// gera lista de funcionarios // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_funcionario");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+$nome=$registro[1];
+echo "<option value=\"$cod\">$cod - $nome<option>\n";
+}
+?>
+</select><br><br>
+
+
+
+
+<br> Codigo pagamento: <select size="1" name="tpg_codigo">
+<?php
+// gera lista de pagamentos // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_tipopagto");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+$nome=$registro[1];
+echo "<option value=\"$cod\">$cod - $nome<option>\n";
+}
+?>
+</select><br><br>
+
+
+
+Codigo Nota:<select size="1" name="nf_codigo">
+<?php
+// gera lista de nfs // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_notafiscal");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+
+Data venda:
+<input type="text" name="ven_datavenda"><br>
+<br>
+
+Valor total:
+<input type="text" name="ven_valortotal"><br>
+<br>
+
+Observações:
+<input type="text" name="ven_observacoes"><br>
+<br>
+
+<input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
