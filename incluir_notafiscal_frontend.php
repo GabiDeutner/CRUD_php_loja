69b0e5baf2f3d1be15cf4b1e3607fff556e36104
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 19:18:23 -0300

    Create incluir_notafiscal_frontend.php

---
+++ incluir_notafiscal_frontend.php
@@ -0,0 +1,20 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_notafiscal_backend.php"><br>
+<p>
+Codigo:
+<input type="text" name="nf_codigo" ><br>
+<br>
+Valor:
+<input type="text" name="nf_valornf"><br>
+<br>
+Taxa imposto:
+<input type="text" name="nf_taxaimpostonf"><br>
+<br>
+Valor imposto:
+<input type="text" name="nf_valorimposto"><br>
+<br>
+<input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
