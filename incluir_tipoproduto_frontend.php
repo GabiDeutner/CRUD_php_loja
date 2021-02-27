5aca07a74edf2667e51e0bf778cbed5d4e161162
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 20:30:43 -0300

    Create incluir_tipoproduto_frontend.php

---
+++ incluir_tipoproduto_frontend.php
@@ -0,0 +1,17 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_tipoproduto_backend.php"><br>
+<p>
+
+Codigo:
+<input type="text" name="tpp_codigo" ><br>
+<br>
+
+Descrição:
+<input type="text" name="tpp_descricao"><br>
+<br>
+
+<input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
