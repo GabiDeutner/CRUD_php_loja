a6527722ff1a0bccfebfd3cd59353dc2be66526e
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 19:18:03 -0300

    Create incluir_contareceber_frontend.php

---
+++ incluir_contareceber_frontend.php
@@ -0,0 +1,42 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_contareceber_backend.php"><br>
+    <p>
+
+Codigo:
+    <input type="text" name="cr_codigo"><br>
+    <br>
+
+Valor Conta:
+    <input type="text" name="cr_valorconta"><br>
+    <br>
+
+Data de vencimento:
+    <input type="text" name="cr_datavencimento"><br>
+    <br>
+
+Data de recebimento:
+    <input type="text" name="cr_datarecebimento"><br>
+    <br>
+
+Codigo Nota Fiscal:
+    <select size="1" name="nf_codigo">
+    <?php
+        // gera lista de notas fiscais// fazer a conexão
+        include "conexao.php";
+        $res = mysql_query ("select * from tb_notafiscal");
+        while ($registro = mysql_fetch_row ($res))
+        {
+            $cod=$registro[0];
+            echo "<option value=\"$cod\">$cod<option>\n";
+        }
+    ?>
+    </select><br><br>
+
+Observações:
+    <input type="text" name="cr_observacoes"><br>
+    <br>
+    <input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
