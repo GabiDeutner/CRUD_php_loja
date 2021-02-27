cf0a220b2b8d80dfcd9a9d3cabf295181c826820
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-02-27 20:30:31 -0300

    Create incluir_tipopagto_frontend.php

---
+++ incluir_tipopagto_frontend.php
@@ -0,0 +1,25 @@
+<!--Início Incluir-->
+<center>
+<form method="post" action="incluir_tipopagto_backend.php"><br>
+<p>
+
+Codigo:
+<input type="text" name="tpg_codigo" ><br>
+<br>
+
+Descrição:
+<input type="text" name="tpg_descricao"><br>
+<br>
+
+Quantidade:
+<input type="text" name="tpg_qtde"><br>
+<br>
+
+Ativo:
+<input type="text" name="tpg_ativo"><br>
+<br>
+
+<input type="submit" name="Submit" value="Incluir">
+</form>
+</center>
+<!--Fim Incluir-->
