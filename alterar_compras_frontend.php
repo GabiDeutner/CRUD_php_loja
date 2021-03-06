a2e79e7dba2c3ef43f935543672f99446160b859
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 15:38:18 -0300

    Create alterar_compras_frontend.php

---
+++ alterar_compras_frontend.php
@@ -0,0 +1,80 @@
+<!--Início alterar-->
+<center>
+<form method="post" action="alterar_compras_backend.php"><br>
+<p>
+Codigo:  <select size="1" name="com_codigo">
+
+<?php
+// gera lista de compras // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_compras");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+
+
+<br> Codigo pagamento: <select size="1" name="tpg_codigo">
+
+<?php
+// gera lista de cidades // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_tipopagto");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+$nome=$registro[1];
+echo "<option value=\"$cod\">$cod - $nome<option>\n";
+}
+?>
+</select><br><br>
+
+Codigo fornecedor:<select size="1" name="for_codigo">
+
+<?php
+// gera lista de cidades // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_fornecedor");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+$nome=$registro[1];
+echo "<option value=\"$cod\">$cod - $nome<option>\n";
+}
+?>
+</select><br><br>
+
+
+
+Codigo Nota:<select size="1" name="nf_codigo">
+
+<?php
+// gera lista de cidades // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_notafiscal");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+
+Data compra:
+<input type="text" name="com_datacompra"><br>
+<br>
+Valor total:
+<input type="text" name="com_valortotal"><br>
+<br>
+Observações:
+<input type="text" name="com_observacoes"><br>
+<br>
+<input type="submit" name="Submit" value="alterar">
+</form>
+</center>
+<!--Fim alterar-->
