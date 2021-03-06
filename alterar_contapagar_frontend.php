75eb71306ff6e57632a1357516708db31d878ab5
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 13:25:11 -0300

    Create alterar_contapagar_frontend.php

---
+++ alterar_contapagar_frontend.php
@@ -0,0 +1,57 @@
+<!--Início alterar-->
+<center>
+<form method="post" action="alterar_contapagar_backend.php"><br>
+    <p>
+
+Codigo: <select size="1" name="cr_codigo">
+
+<?php
+// gera lista de conta pagar // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_contapagar");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+
+
+Valor Conta:
+    <input type="text" name="cp_valorconta"><br>
+    <br>
+
+Data de Vencimento:
+    <input type="text" name="cp_datavencimento"><br>
+    <br>
+
+Data de Pagamento:
+    <input type="text" name="cp_datapagamento"><br>
+    <br>
+
+Codigo Nota Fiscal:
+<select size="1" name="nf_codigo">
+
+<?php
+    // gera lista de notas fiscais // fazer a conexão
+    include "conexao.php";
+    $res = mysql_query ("select * from tb_notafiscal");
+    while ($registro = mysql_fetch_row ($res))
+        {
+            $cod=$registro[0];
+            echo "<option value=\"$cod\">$cod<option>\n";
+        }
+?>
+</select><br><br>
+
+
+
+Observações:
+<input type="text" name="cp_observacoes"><br>
+<br>
+<input type="submit" name="Submit" value="alterar">
+</form>
+</center>
+<!--Fim alterar-->
