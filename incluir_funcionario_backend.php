cb512431306f4bdfc300a7406a45daa72e572b09
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 17:03:47 -0300

    Update incluir_funcionario_backend.php

--- incluir_funcionario_backend.php
+++ incluir_funcionario_backend.php
@@ -1,6 +1,6 @@
 <?php
 include "conexao.php";
-$for_codigo1=$_POST['fun_codigo'];
+$fun_codigo1=$_POST['fun_codigo'];
 $fun_nome1=$_POST['fun_nome'];
 $fun_endereco1=$_POST['fun_endereco'];
 $fun_numero1=$_POST['fun_numero'];
@@ -23,10 +23,10 @@ $inserir = mysql_query("INSERT INTO tb_funcionario
           fun_uf, fun_cnpjcpf, fun_rgie, fun_sexo,
           fun_nascimento, fun_telefone,
           for_celular, for_email, fun_salario)
-           VALUES('$for_codigo1','$for_endereco1',
-		   '$for_numero1','$for_bairro1','$for_cidade1',
-		   '$for_uf1','$for_cnpjcpf1', '$for_rgie1', 
-           '$for_telefone1','$for_fax1', '$for_celular1', '$for_email1')") 
+           VALUES('$fun_codigo1','$fun_nome1','$fun_endereco1','$fun_numero1',
+           '$fun_complemento1','$fun_bairro1', '$fun_cidade1', '$fun_uf1',
+            '$fun_cnpjcpf1','$fun_rgie1','$fun_sexo1','$fun_nascimento1','$fun_telefone1',
+             '$fun_celular1', '$fun_email1', '$fun_salario1')") 
 		   or die (mysql_error());
 mysql_close ($db);
 include "consulta_funcionario.php";
