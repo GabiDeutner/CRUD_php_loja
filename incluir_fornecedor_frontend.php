4134c35856d7180600f0113e4137e1da848752b2
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 19:04:40 -0300

    Update incluir_fornecedor_frontend.php

--- incluir_fornecedor_frontend.php
+++ incluir_fornecedor_frontend.php
@@ -3,57 +3,57 @@
 <form method="post" action="incluir_fornecedor_backend.php"><br>
 <p>
 
-Código:
-    <input type="text" name="for_codigo" ><br>
-    <br>
+Codigo:
+<input type="text" name="for_codigo" ><br>
+<br>
 
 Nome:
-    <input type="text" name="for_nome" ><br>
-    <br>
+<input type="text" name="for_nome"><br>
+<br>
 
 Endereço:
-    <input type="text" name="for_endereco" ><br>
-    <br>
+<input type="text" name="for_endereco"><br>
+<br>
 
-Número:
-    <input type="text" name="for_numero" ><br>
-    <br>
+Numero:
+<input type="text" name="for_numero"><br>
+<br>
 
 Bairro:
-    <input type="text" name="for_bairro" ><br>
-    <br>
+<input type="text" name="for_bairro"><br>
+<br>
 
 Cidade:
-    <input type="text" name="for_cidade" ><br>
-    <br>
+<input type="text" name="for_cidade"><br>
+<br>
 
 UF:
-    <input type="text" name="for_uf" ><br>
-    <br>
+<input type="text" name="for_uf"><br>
+<br>
 
-CNPJCPF:
-    <input type="text" name="for_cnpjcpf" ><br>
-    <br>
+CNPJ ou CPF:
+<input type="text" name="for_cnpjcpf"><br>
+<br>
 
 RG:
-    <input type="text" name="for_rgie" ><br>
-    <br>
+<input type="text" name="for_rgie"><br>
+<br>
 
 Telefone:
-    <input type="text" name="for_telefone" ><br>
-    <br>
+<input type="text" name="for_telefone"><br>
+<br>
 
 Fax:
-    <input type="text" name="for_fax" ><br>
-    <br>
+<input type="text" name="for_fax"><br>
+<br>
 
 Celular:
-    <input type="text" name="for_celular" ><br>
-    <br>
+<input type="text" name="for_celular"><br>
+<br>
 
 E-mail:
-    <input type="text" name="for_email" ><br>
-    <br>
+<input type="text" name="for_email"><br>
+<br>
 
 <input type="submit" name="Submit" value="Incluir">
 </form>
