6476f3c3e86ba5691134f372f67b6561ebdb04c8
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 17:48:05 -0300

    Create alterar_tipoproduto_frontend.php

---
+++ alterar_tipoproduto_frontend.php
@@ -0,0 +1,27 @@
+<!--Início alterar-->
+<center>
+<form method="post" action="alterar_tipoproduto_backend.php"><br>
+<p>
+
+Codigo: <select size="1" name="tpp_codigo">
+
+<?php
+// gera lista de tipo produto // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_tipoproduto");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+Descrição:
+<input type="text" name="tpp_descricao"><br>
+<br>
+
+<input type="submit" name="Submit" value="alterar">
+</form>
+</center>
+<!--Fim alterar-->
