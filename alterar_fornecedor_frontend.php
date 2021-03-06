1d69bca1fcaccfb8207e62c1e6526f322afcd3bf
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-06 13:51:12 -0300

    Create alterar_fornecedor_frontend.php

---
+++ alterar_fornecedor_frontend.php
@@ -0,0 +1,71 @@
+<!--Início alterar-->
+<center>
+<form method="post" action="alterar_fornecedor_backend.php"><br>
+<p>
+
+Código: <select size="1" name="for_codigo">
+
+<?php
+// gera lista de fornecedor // fazer a conexão
+include "conexao.php";
+$res = mysql_query ("select * from tb_fornecedor");
+while ($registro = mysql_fetch_row ($res))
+{
+$cod=$registro[0];
+echo "<option value=\"$cod\">$cod<option>\n";
+}
+?>
+</select><br><br>
+
+Nome:
+    <input type="text" name="for_nome" ><br>
+    <br>
+
+Endereço:
+    <input type="text" name="for_endereco" ><br>
+    <br>
+
+Número:
+    <input type="text" name="for_numero" ><br>
+    <br>
+
+Bairro:
+    <input type="text" name="for_bairro" ><br>
+    <br>
+
+Cidade:
+    <input type="text" name="for_cidade" ><br>
+    <br>
+
+UF:
+    <input type="text" name="for_uf" ><br>
+    <br>
+
+CNPJCPF:
+    <input type="text" name="for_cnpjcpf" ><br>
+    <br>
+
+RG:
+    <input type="text" name="for_rgie" ><br>
+    <br>
+
+Telefone:
+    <input type="text" name="for_telefone" ><br>
+    <br>
+
+Fax:
+    <input type="text" name="for_fax" ><br>
+    <br>
+
+Celular:
+    <input type="text" name="for_celular" ><br>
+    <br>
+
+E-mail:
+    <input type="text" name="for_email" ><br>
+    <br>
+
+<input type="submit" name="Submit" value="alterar">
+</form>
+</center>
+<!--Fim alterar-->
