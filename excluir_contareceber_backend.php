d0b2bcf0db4f1fa2e46579b4cb3b45b41468a495
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-13 10:49:50 -0300

    Create excluir_contareceber_backend.php

---
+++ excluir_contareceber_backend.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+include "conexao.php";
+$cr_codigo1=$_POST['cr_codigo'];
+
+$excluir = mysql_query("DELETE FROM tb_notafiscal
+			 WHERE cr_codigo = '$cr_codigo1'")
+			or die(mysql_error());
+mysql_close ($db);
+include "consulta_contareceber.php";
+?>
