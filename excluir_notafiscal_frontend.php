e81dbb439f8010825f5be64226aebd5ae40ba323
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:14:51 -0300

    Update excluir_notafiscal_frontend.php

--- excluir_notafiscal_frontend.php
+++ excluir_notafiscal_frontend.php
@@ -1,5 +1,6 @@
 <!-- Inicio Excluir -->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Excluir Nota Fiscal </h1>
 <center>
 <form method="post" action="excluir_notafiscal_backend.php">
 <br> Codigo: <select size="1" name="nf_codigo">
