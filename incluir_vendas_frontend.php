175f60bb6fafc983d40e380cc4f2369a72b87f8b
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 00:01:29 -0300

    Update incluir_vendas_frontend.php

--- incluir_vendas_frontend.php
+++ incluir_vendas_frontend.php
@@ -1,4 +1,5 @@
 <!--Início Incluir-->
+<link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
 <center>
 <form method="post" action="incluir_vendas_backend.php"><br>
 <p>
@@ -69,7 +70,7 @@ Observações:
 <input type="text" name="ven_observacoes"><br>
 <br>
 
-<input type="submit" name="Submit" value="Incluir">
+<input type="submit" name="Submit" value="Incluir" class="botao">
 </form>
 </center>
 <!--Fim Incluir-->
