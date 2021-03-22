1ac40a6ee8ba85de5a196f25d396de01a70ec90a
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:14:43 -0300

    Update excluir_itensvenda_frontend.php

--- excluir_itensvenda_frontend.php
+++ excluir_itensvenda_frontend.php
@@ -1,5 +1,6 @@
 <!-- Inicio Excluir -->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Excluir Itens Venda </h1>
 <center>
 <form method="post" action="excluir_itensvenda_backend.php">
 <br> Codigo: <select size="1" name="itensvenda_codigo">
