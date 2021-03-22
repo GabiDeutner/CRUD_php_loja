c7b561d182f5675979cdee0034c5a6f8d3d6b0d9
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 16:09:43 -0300

    Update consulta_vendas.php

--- consulta_vendas.php
+++ consulta_vendas.php
@@ -1,16 +1,17 @@
+<link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
 <?php
 error_reporting(0);
 include "conexao.php";
 $result = mysql_query("select * from tb_vendas", $db);
 echo "<center><table border='1'>
 <tr>
-	<td><h1>Codigo</h1></td>
-    <td><h1>Codigo Funcionario</h1></td>
-	<td><h1>Codigo Tipo Pagamento</h1></td>
-	<td><h1>Codigo Nota Fiscal</h1></td>
-    <td><h1>Data Venda</h1></td>
-    <td><h1>Valor Total</h1></td>
-    <td><h1>Observacoes</h1></td>
+	<td class = 'table_titulo'><h3>Codigo</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Funcionario</h3></td>
+	<td class = 'table_titulo'><h3>Codigo Tipo Pagamento</h3></td>
+	<td class = 'table_titulo'><h3>Codigo Nota Fiscal</h3></td>
+    <td class = 'table_titulo'><h3>Data Venda</h3></td>
+    <td class = 'table_titulo'><h3>Valor Total</h3></td>
+    <td class = 'table_titulo'><h3>Observacoes</h3></td>
 </tr>";
 // Escreve resultado até que não haja mais linhas na tabela
 while($row = mysql_fetch_array($result)) {
