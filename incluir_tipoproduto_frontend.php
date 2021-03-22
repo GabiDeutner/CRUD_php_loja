970859f2535cd165795c7e2d26a4631098fd91a0
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:16:57 -0300

    Update incluir_tipoproduto_frontend.php

--- incluir_tipoproduto_frontend.php
+++ incluir_tipoproduto_frontend.php
@@ -1,5 +1,6 @@
 <!--Início Incluir-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Incluir Tipo de Produto </h1>
 <center>
 <form method="post" action="incluir_tipoproduto_backend.php"><br>
 <p>
