01f7410b1f6204d2a8505b83b1b9fc0789304ead
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:12:52 -0300

    Update alterar_funcionario_frontend.php

--- alterar_funcionario_frontend.php
+++ alterar_funcionario_frontend.php
@@ -1,5 +1,6 @@
 <!--Início alterar-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Alterar Funcionário </h1>
 <center>
 <form method="post" action="alterar_funcionario_backend.php"><br>
 <p>
