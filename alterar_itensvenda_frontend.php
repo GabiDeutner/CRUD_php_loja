a868b4ca63d31b621c1c1fc03ab708161bf00657
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:13:07 -0300

    Update alterar_itensvenda_frontend.php

--- alterar_itensvenda_frontend.php
+++ alterar_itensvenda_frontend.php
@@ -1,5 +1,6 @@
 <!--Início alterar-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Alterar Itens Venda </h1>
 <center>
 <form method="post" action="alterar_itensvenda_backend.php"><br>
 <p>
