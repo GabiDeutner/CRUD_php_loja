88c140b07a3a9c2e7298f705a8b87c40c3db7d99
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 01:06:02 -0300

    Update incluir_contapagar_frontend.php

--- incluir_contapagar_frontend.php
+++ incluir_contapagar_frontend.php
@@ -1,4 +1,5 @@
 <!--Início Incluir-->
+<link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
 <center>
 <form method="post" action="incluir_contapagar_backend.php"><br>
     <p>
@@ -39,7 +40,7 @@ Codigo Nota Fiscal:
 Observações:
 <input type="text" name="cp_observacoes"><br>
 <br>
-<input type="submit" name="Submit" value="Incluir">
+<input type="submit" name="Submit" value="Incluir" class="botao">
 </form>
 </center>
 <!--Fim Incluir-->
