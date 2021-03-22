66b21e1fb60aed1cd7c773eb5099929fdde438e8
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 18:20:59 -0300

    Update alterar_funcionario_backend.php

--- alterar_funcionario_backend.php
+++ alterar_funcionario_backend.php
@@ -1,8 +1,7 @@
 <?php
 error_reporting(0);
 include "conexao.php";
-
-$for_codigo1=$_POST['fun_codigo'];
+$fun_codigo1=$_POST['fun_codigo'];
 $fun_nome1=$_POST['fun_nome'];
 $fun_endereco1=$_POST['fun_endereco'];
 $fun_numero1=$_POST['fun_numero'];
@@ -19,23 +18,23 @@ $fun_celular1=$_POST['fun_celular'];
 $fun_email1=$_POST['fun_email'];
 $fun_salario1=$_POST['fun_salario'];
 
-$alterar = mysql_query("UPDATE tb_funcionario 
-          SET   fun_nome = '$fun_nome1', 
-                fun_endereco = '$fun_nome1', 
-                fun_numero = '$fun_numero1', 
-                fun_complemento = '$fun_complemento1',
-                fun_bairro = '$fun_bairro1', 
-                fun_cidade = '$fun_cidade1',
-                fun_uf = '$fun_uf1', 
-                fun_cnpjcpf = '$fun_cnpjcpf1', 
-                fun_rgie = '$fun_rgie1', 
-                fun_sexo = '$fun_sexo1',
-                fun_nascimento = '$fun_nascimento1', 
-                fun_telefone = '$fun_telefone1',
-                fun_celular = '$fun_celular1', 
-                fun_email = '$fun_email1', 
-                fun_salario = '$fun_salario1'
-           WHERE fun_codigo = '$fun_codigo1'") 
+$alterar = mysql_query("UPDATE tb_funcionario
+                    SET fun_nome='$fun_nome1',
+                        fun_endereco='$fun_endereco1',
+						fun_numero='$fun_numero1',
+                        fun_complemento='$fun_complemento1',
+						fun_bairro='$fun_bairro1',
+                        fun_cidade='$fun_cidade1',
+                        fun_uf='$fun_uf1',
+                        fun_cnpjcpf='$fun_cnpjcpf1',
+                        fun_rgie='$fun_rgie1',
+                        fun_sexo='$fun_sexo1',
+                        fun_nascimento='$fun_nascimento1',
+                        fun_telefone='$fun_telefone1',
+                        fun_celular='$fun_celular1',
+						fun_email='$fun_email1',
+                        fun_salario='$fun_salario1'					  
+					WHERE fun_codigo='$fun_codigo1'")
 		   or die (mysql_error());
 mysql_close ($db);
 include "consulta_funcionario.php";
