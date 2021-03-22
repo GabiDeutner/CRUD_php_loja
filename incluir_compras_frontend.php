39897b6e02fe1d6d1fd7681523d7d66df31c9029
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 01:05:53 -0300

    Update incluir_compras_frontend.php

--- incluir_compras_frontend.php
+++ incluir_compras_frontend.php
@@ -1,4 +1,5 @@
 <!--Início Incluir-->
+<link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
 <center>
 <form method="post" action="incluir_compras_backend.php"><br>
 <p>
@@ -62,7 +63,7 @@ Valor total:
 Observações:
 <input type="text" name="com_observacoes"><br>
 <br>
-<input type="submit" name="Submit" value="Incluir">
+<input type="submit" name="Submit" value="Incluir" class="botao">
 </form>
 </center>
 <!--Fim Incluir-->
