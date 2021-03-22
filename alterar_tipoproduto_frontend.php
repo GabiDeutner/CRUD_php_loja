df28026118418e8e43aec40394396347d8f099ba
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:13:39 -0300

    Update alterar_tipoproduto_frontend.php

--- alterar_tipoproduto_frontend.php
+++ alterar_tipoproduto_frontend.php
@@ -1,5 +1,6 @@
 <!--Início alterar-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Alterar Tipo de Produto </h1>
 <center>
 <form method="post" action="alterar_tipoproduto_backend.php"><br>
 <p>
