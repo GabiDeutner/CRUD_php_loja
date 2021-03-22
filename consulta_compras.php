1051c6420446a00a3e8ab4d9a10c1a233acae4bf
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 15:10:46 -0300

    Update consulta_compras.php

--- consulta_compras.php
+++ consulta_compras.php
@@ -3,15 +3,15 @@
 error_reporting(0);
 include "conexao.php";
 $result = mysql_query("select * from tb_compras", $db);
-echo "<center><table border='1' class = 'table_titulo'>
+echo "<center><table border='1'>
 <tr>
-	<td><h1>Código</h1></td>
-	<td><h1>Código tipo de pagamento</h1></td>
-	<td><h1>Código do fornecedor</h1></td>
-	<td><h1>Código Nota Fiscal</h1></td>
-	<td><h1>Data de Compra</h1></td>
-	<td><h1>Valor Total</h1></td>
-	<td><h1>Observações</h1></td>
+	<td class = 'table_titulo'><h1>Código</h1></td>
+	<td class = 'table_titulo'><h1>Código tipo de pagamento</h1></td>
+	<td class = 'table_titulo'><h1>Código do fornecedor</h1></td>
+	<td class = 'table_titulo'><h1>Código Nota Fiscal</h1></td>
+	<td class = 'table_titulo'><h1>Data de Compra</h1></td>
+	<td class = 'table_titulo'><h1>Valor Total</h1></td>
+	<td class = 'table_titulo'><h1>Observações</h1></td>
 </tr>";
 // Escreve resultado até que não haja mais linhas na tabela
 while($row = mysql_fetch_array($result)) {
