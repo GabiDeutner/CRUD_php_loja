dfdb8ea9d451b1d31b52d2bd86da6677d206a7c4
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 16:08:55 -0300

    Update consulta_itenscompra.php

--- consulta_itenscompra.php
+++ consulta_itenscompra.php
@@ -1,21 +1,22 @@
+<link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
 <?php
 error_reporting(0);
 include "conexao.php";
 $result = mysql_query("select * from tb_itenscompra", $db);
 echo "<center><table border='1'>
 <tr>
-	<td><h1>Codigo Itens Compra</h1></td>
-    <td><h1>Codigo Compra</h1></td>
-    <td><h1>Codigo Produto</h1></td>
-    <td><h1>Codigo Tipo de Produto</h1></td>
-    <td><h1>Codigo Nota Fiscal</h1></td>
-    <td><h1>Codigo Fornecedor</h1></td>
-    <td><h1>Codigo Tipo de Pagamento</h1></td>
-    <td><h1>Embalagem</h1></td>
-	<td><h1>Quantidade</h1></td>
-	<td><h1>Valor Unidade</h1></td>
-	<td><h1>Desconto</h1></td>
-    <td><h1>Valor Total</h1></td>
+	<td class = 'table_titulo'><h3>Codigo Itens Compra</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Compra</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Produto</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Tipo de Produto</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Nota Fiscal</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Fornecedor</h3></td>
+    <td class = 'table_titulo'><h3>Codigo Tipo de Pagamento</h3></td>
+    <td class = 'table_titulo'><h3>Embalagem</h3></td>
+	<td class = 'table_titulo'><h3>Quantidade</h3></td>
+	<td class = 'table_titulo'><h3>Valor Unidade</h3></td>
+	<td class = 'table_titulo'><h3>Desconto</h3></td>
+    <td class = 'table_titulo'><h3>Valor Total</h3></td>
 </tr>";
 // Escreve resultado até que não haja mais linhas na tabela
 while($row = mysql_fetch_array($result)) {
