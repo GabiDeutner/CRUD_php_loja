c929e5e88963f610713b37a4eca04dd613ff4eb3
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:13:31 -0300

    Update alterar_tipopagto_frontend.php

--- alterar_tipopagto_frontend.php
+++ alterar_tipopagto_frontend.php
@@ -1,5 +1,6 @@
 <!--Início alterar-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Alterar Tipo de Pagamento </h1>
 <center>
 <form method="post" action="alterar_tipopagto_backend.php"><br>
 <p>
