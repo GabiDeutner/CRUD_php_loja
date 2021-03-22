c4103127dc21dac2d3539abeffbb8817f5aaea4f
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-22 17:16:31 -0300

    Update incluir_notafiscal_frontend.php

--- incluir_notafiscal_frontend.php
+++ incluir_notafiscal_frontend.php
@@ -1,5 +1,6 @@
 <!--Início Incluir-->
 <link rel="stylesheet" type="text/css" href="formatarpadrao.css" />
+<h1 class="titulos"> Incluir Nota Fiscal </h1>
 <center>
 <form method="post" action="incluir_notafiscal_backend.php"><br>
 <p>
