6120c921b3b9ba38ca5a98435b7daab1c5ab3ca9
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-03-24 00:58:30 -0300

    Update consulta_vendas.php

--- consulta_vendas.php
+++ consulta_vendas.php
@@ -6,13 +6,13 @@ include "conexao.php";
 $result = mysql_query("select * from tb_vendas", $db);
 echo "<center><table border='1'>
 <tr>
-	<td class = 'table_titulo'><h3>Codigo</h3></td>
-    <td class = 'table_titulo'><h3>Codigo Funcionario</h3></td>
-	<td class = 'table_titulo'><h3>Codigo Tipo Pagamento</h3></td>
-	<td class = 'table_titulo'><h3>Codigo Nota Fiscal</h3></td>
+	<td class = 'table_titulo'><h3>Código Venda</h3></td>
+    <td class = 'table_titulo'><h3>Código Funcionário</h3></td>
+	<td class = 'table_titulo'><h3>Código Tipo Pagamento</h3></td>
+	<td class = 'table_titulo'><h3>Código Nota Fiscal</h3></td>
     <td class = 'table_titulo'><h3>Data Venda</h3></td>
     <td class = 'table_titulo'><h3>Valor Total</h3></td>
-    <td class = 'table_titulo'><h3>Observacoes</h3></td>
+    <td class = 'table_titulo'><h3>Observações</h3></td>
 </tr>";
 // Escreve resultado até que não haja mais linhas na tabela
 while($row = mysql_fetch_array($result)) {
